47fc0d2b31a0cfcf3d9764ced33f7fbd3d2358b1
HieuTong <coinhako@Coinhakos-MacBook-Pro.local>
2020-05-20 14:28:25 +0700

    mockup-ui: flow navigation in app

--- App.js
+++ App.js
@@ -12,14 +12,17 @@ import CourseDetail from './src/components/CourseDetail/course-detail';
 import NavigationContainer from '@react-navigation/native/src/NavigationContainer';
 import { createStackNavigator } from '@react-navigation/stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import {navigationName} from './src/globals/constants'
-import {headerStyle} from './src/globals/styles'
-import {colorConst} from './src/globals/constants'
-const Stack = createStackNavigator()
+import { navigationName } from './src/globals/constants'
+import { headerStyle } from './src/globals/styles'
+import { colorConst } from './src/globals/constants'
+import Splash from './src/components/Authentication/Splash/splash';
+import Register from './src/components/Authentication/Register/register';
+import Profile from './src/AccountManagement/Account/profile';
 
+const Stack = createStackNavigator()
 const ListCoursesStack = () => {
-  return(
-<Stack.Navigator initialRouteName = "ListCourses" screenOptions = {{
+  return (
+    <Stack.Navigator initialRouteName="ListCourses" screenOptions={{
       headerStyle: {
         backgroundColor: colorConst.colorHeader,
       },
@@ -27,18 +30,18 @@ const ListCoursesStack = () => {
       headerTitleStyle: {
         fontWeight: 'bold',
       },
-}} mode = "modal"
-      >
-        <Stack.Screen name = {navigationName.ListCourse} component = {ListCourses} options = {{
-      title: 'List Course',
-      headerRight: () => (
-        <Button
-          onPress={() => Alert.alert('Setting', 'This is the setting screens')}
-          title = "Info"
-          color = "#fff"
-        />
-      ),
-      }} 
+    }} mode="modal"
+    >
+      <Stack.Screen name={navigationName.ListCourse} component={ListCourses} options={{
+        title: 'List Course',
+        headerRight: () => (
+          <Button
+            onPress={() => Alert.alert('Setting', 'This is the setting screens')}
+            title="Info"
+            color="#fff"
+          />
+        ),
+      }}
       />
       <Stack.Screen name="CourseDetail" component={CourseDetail} options={
         // ({route}) => {
@@ -49,61 +52,56 @@ const ListCoursesStack = () => {
           headerShown: false,
         }
       } />
-      </Stack.Navigator>
+    </Stack.Navigator>
   )
 }
 
-const ListCoursesScreen = () => {
-  
-}
-
-const DetailCourseScreen = () => {
-  
-}
-// const HomeStack  = () => {
-//   return(
-//     <Stack.Screen>
-//         <ListCoursesStack />
-//         <DetailCourseScreen />
-
-//     </Stack.Screen>
-//   )
-// }
-
-
+const AuthStack = createStackNavigator();
+const AuthStackScreen = () => (
+  <AuthStack.Navigator>
+    <AuthStack.Screen name="SignIn" component={Login} options={{ title: "Sign In", headerShown: false }} />
+    <AuthStack.Screen name="Register" component={Register} options={{ title: "Register", headerShown: false }} />
+  </AuthStack.Navigator>
+)
 
 
 const Tab = createBottomTabNavigator()
+const TabBottomScreen = () => (
+  <Tab.Navigator>
+    <Tab.Screen name="Home" component={Home} />
+    <Tab.Screen name="ListCourses" component={ListCoursesStack} options={{ title: "Explorer" }} />
+    <Tab.Screen name="Profile" component={Profile} />
 
+    {/* <Tab.Screen name="Download" component={Download} />
+    <Tab.Screen name="Browse" component={Browse} />
+    <Tab.Screen name="Search" component={SearchCourses} options={{
+      headerShown: false,
+    }} /> */}
+
+  </Tab.Navigator>
+)
+
+const MainStack = createStackNavigator()
+const MainStackScreen = () => (
+  <MainStack.Navigator>
+    <MainStack.Screen name="SignIn" component={Login} options={{ headerShown: false }} />
+    <MainStack.Screen name="Splash" component={Splash} options={{ headerShown: false }} />
+    <MainStack.Screen name="MainApp" component={TabBottomScreen} options={{ headerShown: false }} />
+  </MainStack.Navigator>
+)
 
 export default function App() {
-  
   return (
     <NavigationContainer>
-        <Tab.Navigator>
-        <Tab.Screen name = "Home" component={Home} />
-        <Tab.Screen name = "ListCourses" component = {ListCoursesStack}/>
-        <Tab.Screen name = "Download" component={Download} />
-        <Tab.Screen name = "Browse" component={Browse} />
-        <Tab.Screen name = "Search" component={SearchCourses} options = {{
-          headerShown: false,
-        }} />
-      </Tab.Navigator>
+      <MainStackScreen />
     </NavigationContainer>
-    // <View style={styles.container}>
-    //   {/* <SearchCourses /> */}
-    //   {/* <Browse /> */}
-    //   {/* <Download /> */}
-    //   {/* <ListCourses /> */}
-    //   {/* <Home /> */}
-    //   {/* <Login /> */}
-    // </View>
+
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    marginTop: 30,    
+    marginTop: 30,
   },
 });
