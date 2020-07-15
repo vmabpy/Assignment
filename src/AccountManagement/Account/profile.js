3c2729e5796f5cf39f3da795ba0d14b60cc47d1c
HieuTong <hieuminhtongle20111998@gmail.com>
2020-07-15 14:08:29 +0700

    apiLogin

--- src/AccountManagement/Account/profile.js
+++ src/AccountManagement/Account/profile.js
@@ -1,100 +1,102 @@
-import React, { useContext } from 'react'
-import { Text, View, StyleSheet, Image } from 'react-native'
-import { AuthenticationContext } from '../../provider/authentication-provider'
-import { ScrollView } from 'react-native-gesture-handler'
+import React, { useContext } from "react";
+import { Text, View, StyleSheet, Image } from "react-native";
+import { AuthenticationContext } from "../../provider/authentication-provider";
+import { ScrollView } from "react-native-gesture-handler";
 
 const Profile = (props) => {
-    const item = props.route.params.item
-    const { authentication } = useContext(AuthenticationContext)
+  const item = props.route.params.item;
+  const { state } = useContext(AuthenticationContext);
 
-    return (
-        // <View style={styles.container}>
-        //     {/* <Text>{item.username}</Text>
-        //     <Text>{item.fullName}</Text> */}
-        //     <Text>{authentication.user.username}</Text>
-        //     <Text>{authentication.user.fullName}</Text>
+  return (
+    // <View style={styles.container}>
+    //     {/* <Text>{item.username}</Text>
+    //     <Text>{item.fullName}</Text> */}
+    //     <Text>{authentication.user.username}</Text>
+    //     <Text>{authentication.user.fullName}</Text>
 
-        // </View>
-        <ScrollView>
-            <View style={styles.container}>
-                <View style={{ alignItems: 'center' }}>
-                    <Image source={require('../../../assets/ic_profile.png')} style={styles.image} />
-                    <Text style={styles.name}>{authentication.user.username}</Text>
-                </View>
+    // </View>
+    <ScrollView>
+      <View style={styles.container}>
+        <View style={{ alignItems: "center" }}>
+          <Image
+            source={require("../../../assets/ic_profile.png")}
+            style={styles.image}
+          />
+          <Text style={styles.name}>{state.userInfo.name}</Text>
+          {/* <Text style={styles.name}>Hieu Tong</Text> */}
+        </View>
 
-                {/* <Text>{authentication.user.fullName}</Text> */}
-                <View style={styles.view}>
-                    <Text style={styles.title}>Activity insights (last 30 days)</Text>
-                    <Text style={styles.subTitle}>TOTAL ACTIVE DAYS</Text>
-                    <Text style={styles.title}>4 days streak</Text>
-                    <Text style={styles.subTitle}>MOST ACTIVE TIME OF DAY</Text>
-                    <Text style={styles.title}>14:00</Text>
-                    <Text style={styles.subTitle}>MOST VIEW SUBJECT</Text>
-                    <Text style={styles.title}>React Native</Text>
-                </View>
-            </View>
-        </ScrollView>
-    )
-    // return (
-    //     <AuthenticationContext.Consumer>
-    //         {
-    //             ({ authentication }) => {
-    //                 console.log('profile authentication: ', authentication)
-    //                 return (
-    //                     <ThemeContext.Consumer>
-    //                         {
-    //                             ({ theme, setTheme }) => {
-    //                                 return (
-    //                                     <View style={{ ...styles.container, backgroundColor: theme.background }}>
-    //                                         {/* <Text>{item.username}</Text>
-    //                                         <Text>{item.fullName}</Text> */}
-    //                                         <Text>{authentication.user.username}</Text>
-    //                                         <Text>{authentication.user.fullName}</Text>
-    //                                     </View>
-    //                                 )
-    //                             }
-    //                         }
-    //                     </ThemeContext.Consumer>
+        {/* <Text>{authentication.user.fullName}</Text> */}
+        <View style={styles.view}>
+          <Text style={styles.title}>Activity insights (last 30 days)</Text>
+          <Text style={styles.subTitle}>TOTAL ACTIVE DAYS</Text>
+          <Text style={styles.title}>4 days streak</Text>
+          <Text style={styles.subTitle}>MOST ACTIVE TIME OF DAY</Text>
+          <Text style={styles.title}>14:00</Text>
+          <Text style={styles.subTitle}>MOST VIEW SUBJECT</Text>
+          <Text style={styles.title}>React Native</Text>
+        </View>
+      </View>
+    </ScrollView>
+  );
+  // return (
+  //     <AuthenticationContext.Consumer>
+  //         {
+  //             ({ authentication }) => {
+  //                 console.log('profile authentication: ', authentication)
+  //                 return (
+  //                     <ThemeContext.Consumer>
+  //                         {
+  //                             ({ theme, setTheme }) => {
+  //                                 return (
+  //                                     <View style={{ ...styles.container, backgroundColor: theme.background }}>
+  //                                         {/* <Text>{item.username}</Text>
+  //                                         <Text>{item.fullName}</Text> */}
+  //                                         <Text>{authentication.user.username}</Text>
+  //                                         <Text>{authentication.user.fullName}</Text>
+  //                                     </View>
+  //                                 )
+  //                             }
+  //                         }
+  //                     </ThemeContext.Consumer>
 
-    //                 )
-    //             }
-    //         }
-    //     </AuthenticationContext.Consumer>
-    // )
-
-}
+  //                 )
+  //             }
+  //         }
+  //     </AuthenticationContext.Consumer>
+  // )
+};
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-    },
-    image: {
-        marginTop: 50,
-        height: 120,
-        width: 120,
-        borderRadius: 60,
-    },
-    name: {
-        marginTop: 10,
-        fontSize: 16,
-        fontWeight: 'bold',
-
-    },
-    view: {
-        alignItems: 'flex-start',
-        marginLeft: 5,
-        marginTop: 20,
-    },
-    subTitle: {
-        marginTop: 10,
-        fontSize: 14,
-        color: 'gray',
-    },
-    title: {
-        marginTop: 10,
-        fontSize: 16,
-        fontWeight: 'bold',
-    }
-})
+  container: {
+    flex: 1,
+  },
+  image: {
+    marginTop: 50,
+    height: 120,
+    width: 120,
+    borderRadius: 60,
+  },
+  name: {
+    marginTop: 10,
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+  view: {
+    alignItems: "flex-start",
+    marginLeft: 5,
+    marginTop: 20,
+  },
+  subTitle: {
+    marginTop: 10,
+    fontSize: 14,
+    color: "gray",
+  },
+  title: {
+    marginTop: 10,
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+});
 
 export default Profile;
---
+++ src/action/authentication-action.js
@@ -0,0 +1,31 @@
+import axios from "axios";
+import apiLogin from "../core/service/authenticate-service";
+export const LOGIN_REQUEST = "LOGIN_REQUEST";
+export const LOGIN_SUCCESSED = "LOGIN_SUCCESSED";
+export const LOGIN_FAILED = "LOGIN_FAILED";
+
+const loginSuccess = (data) => {
+  return {
+    type: LOGIN_SUCCESSED,
+    data,
+  };
+};
+
+const loginFailed = () => {
+  return { type: LOGIN_FAILED };
+};
+
+export const login = (dispatch) => (username, password) => {
+  dispatch({ type: LOGIN_REQUEST });
+  apiLogin(username, password)
+    .then((response) => {
+      if (response.status === 200) {
+        dispatch(loginSuccess(response.data));
+      } else {
+        dispatch(loginFailed());
+      }
+    })
+    .catch((error) => {
+      dispatch({ type: LOGIN_FAILED });
+    });
+};
